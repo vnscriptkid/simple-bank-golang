46d454cfefaee753cbdd03fd1decef13590aedec
Thanh Nguyen <vnscriptkid@gmail.com>
2022-12-10 12:03:11 +0800

    feat(account): CRUD

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -64,4 +65,58 @@ func TestGetAccount(t *testing.T) {
 	assert.Equal(t, acc.Currency, gotAcc.Currency)
 	assert.Equal(t, acc.Owner, gotAcc.Owner)
 	assert.WithinDuration(t, acc.CreatedAt, gotAcc.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	acc := createFakeAccount(t)
+
+	params := UpdateAccountParams{
+		ID:      acc.ID,
+		Balance: acc.Balance + 1,
+	}
+
+	updatedAcc, err := testQueries.UpdateAccount(context.Background(), params)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, updatedAcc)
+
+	assert.Equal(t, acc.ID, updatedAcc.ID)
+	assert.NotEqual(t, acc.Balance, updatedAcc.Balance)
+	assert.Equal(t, acc.Currency, updatedAcc.Currency)
+	assert.Equal(t, acc.Owner, updatedAcc.Owner)
+	assert.Equal(t, acc.CreatedAt, updatedAcc.CreatedAt)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	acc := createFakeAccount(t)
+
+	err1 := testQueries.DeleteAccount(context.Background(), acc.ID)
+
+	assert.NoError(t, err1)
+
+	foundAcc, err2 := testQueries.GetAccount(context.Background(), acc.ID)
+
+	assert.Error(t, err2)
+	assert.EqualError(t, err2, sql.ErrNoRows.Error())
+	assert.Empty(t, foundAcc)
+}
+
+func TestListAccount(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createFakeAccount(t)
+	}
+
+	params := ListAccountsParams{
+		Offset: 5,
+		Limit:  5,
+	}
+
+	accList, err := testQueries.ListAccounts(context.Background(), params)
+
+	assert.NoError(t, err)
+	assert.Len(t, accList, 5)
+
+	for _, acc := range accList {
+		assert.NotEmpty(t, acc)
+	}
 }
